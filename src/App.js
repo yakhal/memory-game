83e88132e886749e9fca00fb82a58112bc3fbf4a
yakhal <rastogi.p39@gmail.com>
2022-03-16 21:17:40 +0530

    Developed Game Grid UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import GameHeader from "./game-screen-components/GameHeader";
+import GridCircle from "./game-setup-components/GridCircle";
 // import GameSetup from './game-setup-components/GameSetup';
 
 function App() {
+  const gridCircles = [];
+  for (let i=0; i<36; i++){
+    gridCircles.push(<GridCircle/>)
+  }
   return (
     <div className="App">
       {/* <GameSetup/> */}
       <div className="game-container">
         <div className="game">
           <GameHeader/>
+          <div className="game-grid game-grid-6x6">
+            {gridCircles}
+          </div>
         </div>
       </div>
     </div>
---
+++ src/game-setup-components/GridCircle.js
@@ -0,0 +1,13 @@
+function GridCircle(){
+    return (
+        // <div className="grid-circle">
+        //     <div className="grid-circle-content">5</div>
+        // </div>
+        <div className="grid-circle">
+            <div className="grid-circle-content orange"><span>5</span></div>
+            <div className="grid-circle-cover"></div>
+        </div>
+    );
+}
+
+export default GridCircle;
